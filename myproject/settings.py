492acabd85537916b92a58437621d046038cf1ab
Arthur Lewis <arthur.lewis420@gmail.com>
2024-03-05 15:28:58 +1300

    Added postgres database for my dev settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -75,12 +75,23 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'todosdbdev',
+        'USER': 'postgres',
+        'PASSWORD': 'Cellfone01',
+        'HOST': 'localhost',
+        'PORT':'5432',
+    },
 }
 
 
