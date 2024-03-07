8c8af901e062bf12f864c2dda18c4fd8830dadb5
Arthur Lewis <arthur.lewis420@gmail.com>
2024-03-07 15:45:04 +1300

    Updated settings of database and debug for deployment

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,16 +84,28 @@ DATABASES = {
     }
 }
 '''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'todosdbdev',
-        'USER': 'postgres',
-        'PASSWORD': 'Cellfone01',
-        'HOST': 'localhost',
-        'PORT':'5432',
-    },
-}
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+            'default': {
+                'ENGINE':'django.db.backends.postgresql_psycopg2',
+                'NAME': os.environ['RDS_DB_NAME'],
+                'USER': os.environ['RDS_USERNAME'],
+                'PASSWORD': os.environ['RDS_PASSWORD'],
+                'HOST': os.environ['RDS_HOSTNAME'],
+                'PORT': os.environ['RDS_PORT'],
+            }
+        }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'todosdbdev',
+            'USER': 'postgres',
+            'PASSWORD': 'Cellfone01',
+            'HOST': 'localhost',
+            'PORT':'5432',
+        },
+    }
 
 
 # Password validation
